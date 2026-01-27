160a53fd83934971b4ceddd9aac02425bd70d681
ttelcl <9429819+ttelcl@users.noreply.github.com>
2026-01-27 10:53:03 +0200

    Adding initial TteLcl.GitModel.Builder folder

---
+++ TteLcl.GitModel.Builder/GitRepo.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LibGit2Sharp;
+
+using TteLcl.GitModel;
+
+namespace TteLcl.GitModel.Builder;
+
+/// <summary>
+/// Reference to a GIT repository
+/// </summary>
+public class GitRepo
+{
+  /// <summary>
+  /// Create a new <see cref="GitRepo"/> instance referencing an existing git repository
+  /// </summary>
+  /// <param name="witnessPath">
+  /// Any file or folder in an existing normal or bare GIT repository.
+  /// </param>
+  public GitRepo(
+    string witnessPath)
+  {
+    IdCache = new GitIdCache();
+    PathCache = new GitPathCache(IdCache);
+    var dbFolder = FindGitDbFolder(witnessPath);
+    if(String.IsNullOrEmpty(dbFolder))
+    {
+      throw new ArgumentException(
+        $"Expecting the given path to be a file or folder in an existing GIT repository: {witnessPath}",
+        nameof(witnessPath));
+    }
+    GitDbFolder = dbFolder;
+  }
+
+  /// <summary>
+  /// The Git ID cache
+  /// </summary>
+  public GitIdCache IdCache { get; }
+
+  /// <summary>
+  /// The git path cache
+  /// </summary>
+  public GitPathCache PathCache { get; }
+
+  /// <summary>
+  /// The folder containing the git data. For a normal repository that is the ".git" subfolder
+  /// in the repository root. For a bare repository that is the repository folder itself.
+  /// </summary>
+  public string GitDbFolder { get; }
+
+  /// <summary>
+  /// Locate the GIT database folder that contains (or is) the given folder or file
+  /// <paramref name="witness"/>.
+  /// </summary>
+  /// <param name="witness">
+  /// Any file or folder within the git repository to locate.
+  /// </param>
+  /// <returns></returns>
+  /// <exception cref="InvalidOperationException"></exception>
+  public static string? FindGitDbFolder(string witness)
+  {
+    if(!Path.IsPathRooted(witness))
+    {
+      witness = Path.GetFullPath(witness);
+    }
+    witness = witness.TrimEnd(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
+    if(!Directory.Exists(witness))
+    {
+      if(File.Exists(witness))
+      {
+        var witness2 = Path.GetDirectoryName(witness);
+        if(String.IsNullOrEmpty(witness2))
+        {
+          return null;
+        }
+        witness = witness2;
+      }
+      else
+      {
+        throw new InvalidOperationException(
+          "Expecting an existing file or folder as argument");
+      }
+    }
+    // witness now is a folder, as absolute path
+    return FindGitDbFolderInternal(witness);
+  }
+
+  private static string? FindGitDbFolderInternal(string witnessFolder)
+  {
+    witnessFolder = witnessFolder.TrimEnd(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
+    if(String.IsNullOrEmpty(witnessFolder))
+    {
+      return null;
+    }
+    if(!Directory.Exists(witnessFolder))
+    {
+      return null;
+    }
+    // Assumption: witnessFolder is an existing absolute directory without trailing path separator
+    if(IsGitDbFolder(witnessFolder))
+    {
+      return witnessFolder;
+    }
+    var gitChildFolder = Path.Combine(witnessFolder, ".git");
+    if(Directory.Exists(gitChildFolder) && IsGitDbFolder(gitChildFolder))
+    {
+      return gitChildFolder;
+    }
+    var parent = Path.GetDirectoryName(witnessFolder);
+    if(String.IsNullOrEmpty(parent))
+    {
+      return null;
+    }
+    return FindGitDbFolderInternal(parent);
+  }
+
+  private static bool IsGitDbFolder(string folder)
+  {
+    return
+      folder.EndsWith(".git", StringComparison.OrdinalIgnoreCase)
+      && File.Exists(Path.Combine(folder, "config"));
+  }
+}
---
+++ UnitTests.GitModel/BuilderTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+
+using Xunit;
+using Xunit.Abstractions;
+
+using TteLcl.GitModel;
+using TteLcl.GitModel.Builder;
+using System.IO;
+
+namespace UnitTests.GitModel;
+
+public class BuilderTests
+{
+  private readonly ITestOutputHelper _sink;
+
+  public BuilderTests(ITestOutputHelper sink)
+  {
+    _sink = sink;
+  }
+
+  [Fact]
+  public void CanFindGitDbFolder()
+  {
+    // Assumption: this test is run in a folder that is in a git repository
+    var witness = Environment.CurrentDirectory;
+    var gitdbFolder = GitRepo.FindGitDbFolder(witness);
+    Assert.NotNull(gitdbFolder);
+    Assert.True(Directory.Exists(gitdbFolder));
+    Assert.True(File.Exists(Path.Combine(gitdbFolder, "config")));
+    _sink.WriteLine($"Git DB folder for '{witness}' is '{gitdbFolder}'.");
+  }
+
+  [Fact]
+  public void CanDetectNonGitFolder()
+  {
+    var witness = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+    Assert.True(Directory.Exists(witness));
+    var gitdbFolder = GitRepo.FindGitDbFolder(witness);
+    Assert.Null(gitdbFolder);
+    _sink.WriteLine($"Confirmed that '{witness}' is not in any GIT repository");
+  }
+
+  [Fact]
+  public void FindingGitDbIsIdempotent()
+  {
+    // Assumption: this test is run in a folder that is in a git repository
+    var witness = Environment.CurrentDirectory;
+    var gitdbFolder = GitRepo.FindGitDbFolder(witness);
+    Assert.NotNull(gitdbFolder);
+    Assert.True(Directory.Exists(gitdbFolder));
+    Assert.True(File.Exists(Path.Combine(gitdbFolder, "config")));
+
+    var gitDbFolder2 = GitRepo.FindGitDbFolder(gitdbFolder);
+    Assert.Equal(gitdbFolder, gitDbFolder2);
+  }
+}
